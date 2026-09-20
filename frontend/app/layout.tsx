c3757b3933414af79f318e14b92135be9d45f840
Kaushal Gaikwad <kaushalgaikwad810@gmail.com>
2026-09-20 22:42:24 +0530

    add Google Search Console verification

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,7 +17,8 @@ export default function RootLayout({
       <head>
         <meta
           name="google-site-verification"
-          content="<meta name="google-site-verification" content="88azq5cBdUvpAaFzqufHv4vjTXWY2-DF-rxsS1ZDa40" />"
+          content="88azq5cBdUvpAaFzqufHv4vjTXWY2-DF-rxsS1ZDa40"
+        />
       </head>
 
       <body>{children}</body>
